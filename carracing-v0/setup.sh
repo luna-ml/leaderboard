be728c425fe587a9ddbf3e1c221eb8ba8f1f53e7
Lee moon soo <leemoonsoo@gmail.com>
2021-04-06 18:20:15 -0700

    install swig first
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/setup.sh
+++ carracing-v0/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
+apt-get update
+apt-get install -y python-opengl xvfb unzip swig
+
 # install gym from source where carracing is not available in pypi released version
 pushd
 cd /tmp/
@@ -8,7 +12,3 @@ cd gym
 git checkout v0.10.5
 pip install -e '.[box2d]'
 popd
-
-conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
-apt-get update
-apt-get install -y python-opengl xvfb unzip swig
