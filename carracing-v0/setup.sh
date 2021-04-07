367b2e26d4156917bf11e4cbc4138a942908e319
Lee moon soo <leemoonsoo@gmail.com>
2021-04-06 18:11:56 -0700

    install gym from source
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/setup.sh
+++ carracing-v0/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# install gym from source where carracing is not available in pypi released version
+pushd
+cd /tmp/
+git clone https://github.com/openai/gym.git
+cd gym
+git checkout v0.10.5
+pip install -e '.[box2d]'
+popd
+
 conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
 apt-get update
 apt-get install -y python-opengl xvfb unzip swig
