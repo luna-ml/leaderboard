b9282008cd0ed149effabecea79b6b51db9f6c91
Lee moon soo <leemoonsoo@gmail.com>
2021-04-06 18:32:28 -0700

    install gcc
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/setup.sh
+++ carracing-v0/setup.sh
@@ -2,7 +2,7 @@
 
 conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
 apt-get update
-apt-get install -y python-opengl xvfb unzip swig
+apt-get install -y python-opengl xvfb unzip swig gcc
 
 # install gym from source where carracing is not available in pypi released version
 # pushd
