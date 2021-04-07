737fd7cfe42745f4d5b7f84010600405ecff0e8f
Lee moon soo <leemoonsoo@gmail.com>
2021-04-06 18:03:47 -0700

    swig and gym
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/setup.sh
+++ carracing-v0/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
-conda install -y -c anaconda swig
 apt-get update
-apt-get install -y python-opengl xvfb unzip
+apt-get install -y python-opengl xvfb unzip swig
