81dd4c1086eec1b1a47c0575885b9c0f4f9548b5
Lee moon soo <leemoonsoo@gmail.com>
2021-04-13 08:10:05 -0700

    add cartpole a2c pre-trained model (#14)
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

---
+++ cartpole-v1/models/a2c-rl-baselines3-zoo/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd model
+
+# download pretrained model from https://github.com/DLR-RM/rl-trained-agents/blob/d81fcd61cef4599564c859297ea68bacf677db6b/a2c/CartPole-v1_1/CartPole-v1.zip
+wget https://github.com/DLR-RM/rl-trained-agents/raw/d81fcd61cef4599564c859297ea68bacf677db6b/a2c/CartPole-v1_1/CartPole-v1.zip
--- cartpole-v1/models/dqn-ritakurban/setup.sh
+++ cartpole-v1/models/dqn-ritakurban/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# When evaluator runs,
+# the current directory is /workspace, which becomes project root.
+# /workspace/model is where model is mounted to get evaluated.
 cd model
 bunzip2 pretrained.tar.bz2
 tar -xf pretrained.tar
