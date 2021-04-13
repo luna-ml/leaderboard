f96bfc24d5d4bf98aad0da6e8a12fe0558805786
Lee moon soo <leemoonsoo@gmail.com>
2021-04-13 10:20:40 -0700

    udpate setup.sh
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- lunalandercontinuous-v2/setup.sh
+++ lunalandercontinuous-v2/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get update
-apt-get install -y python-opengl xvfb unzip swig
 
 conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
+apt-get update
+apt-get install -y python-opengl xvfb unzip swig gcc build-essential libfontconfig
