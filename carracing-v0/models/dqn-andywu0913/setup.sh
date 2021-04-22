3c1150b815546cd969a0b20df2f250b7c3042373
Lee moon soo <leemoonsoo@gmail.com>
2021-04-22 14:37:00 -0700

    install open cv
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/models/dqn-andywu0913/setup.sh
+++ carracing-v0/models/dqn-andywu0913/setup.sh
@@ -4,6 +4,5 @@ git clone https://github.com/andywu0913/OpenAI-GYM-CarRacing-DQN.git dqn
 cd dqn
 git checkout 778c83685606ee26d0a7fbfbdfe89ebab2268490
 cp *.py ../
-pip install -r requirements.txt
-pip install 'tensorflow<=2.2.0'
+pip install 'tensorflow<=2.2.0' opencv-contrib-python
 pip install 'h5py==2.10.0' --force-reinstall
