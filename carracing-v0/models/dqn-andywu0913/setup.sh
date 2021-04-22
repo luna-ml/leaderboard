bc83e4b2296be412a450fb14cb537fa903fc1209
Lee moon soo <leemoonsoo@gmail.com>
2021-04-22 12:18:52 -0700

    update version
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

--- carracing-v0/models/dqn-andywu0913/setup.sh
+++ carracing-v0/models/dqn-andywu0913/setup.sh
@@ -5,5 +5,5 @@ cd dqn
 git checkout 778c83685606ee26d0a7fbfbdfe89ebab2268490
 cp *.py ../
 pip install -r requirements.txt
-pip install 'tensorflow<=2.0.0'
+pip install 'tensorflow<=2.2.0'
 pip install 'h5py==2.10.0' --force-reinstall
