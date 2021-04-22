1c91612abc20f8826d7e6288fe498399e081a2b8
Lee moon soo <leemoonsoo@gmail.com>
2021-04-22 10:04:53 -0700

    add dqn based agent for carracing
    Signed-off-by: Lee moon soo <leemoonsoo@gmail.com>

---
+++ carracing-v0/models/dqn-andywu0913/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/andywu0913/OpenAI-GYM-CarRacing-DQN.git dqn
+cd dqn
+git checkout 778c83685606ee26d0a7fbfbdfe89ebab2268490
+cp *.py ../
+pip install -r requirements.txt
+pip install 'h5py==2.10.0' --force-reinstall
